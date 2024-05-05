0f363c48b334b95bd799aff2d48c320e660586b6
hiboherson <hiboherson@gmail.com>
2024-05-05 14:47:48 +0300

    commit string lab

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-// Write your code in this file!
-fdnbhk sfvbsdv lwrwkv r]emp 'blevbsf chc fv
-vnsdvbfvhsfvbsfv sfsnvv'
+const currentUser = 'Grace Hopper';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
